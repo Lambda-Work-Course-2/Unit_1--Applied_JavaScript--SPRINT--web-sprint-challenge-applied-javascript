2ea7936cf2db97c6262a2b6c3d21715f652df016
Catherine Tucker <ImCatherineNoel@gmail.com>
2021-01-22 19:52:54 -0800

    Adds compleated tabs.js (Steps 3 and 4
    Bug hunting on 1&2 still

--- src/components/tabs.js
+++ src/components/tabs.js
@@ -1,19 +1,22 @@
+import axios from 'axios';
+
 const Tabs = (topics) => {
-  // TASK 3
-  // ---------------------
-  // Implement this function which takes an array of strings ("topics") as its only argument.
-  // As an example, if the topics passed are ['javascript', 'bootstrap', 'technology']
-  // then the function returns the markup below.
-  // The tags used, the hierarchy of elements and their attributes must match the provided markup!
-  // The text inside elements will be set using their `textContent` property (NOT `innerText`).
-  //
-  // <div class="topics">
-  //   <div class="tab">javascript</div>
-  //   <div class="tab">bootstrap</div>
-  //   <div class="tab">technology</div>
-  // </div>
-  //
-}
+     //create elements to append too
+          const tabMainDiv = document.createElement('div');
+          tabMainDiv.classList.add('topics');
+
+     //Loop to create the tab divs
+          topics.forEach(tabItem => {
+          const tab = document.createElement('div'); //make the tab div
+          tab.classList.add('tab');
+          tab.textContent = tabItem; // give the link a name
+          tabMainDiv.appendChild(tab);  //add the item to the list
+     })
+     //TODO: REMOVE   
+     console.log('file: tabs.js ~ line 28 ~ Tabs ~ tabMainDiv', tabMainDiv);   
+     //return the thing!
+          return(tabMainDiv);  
+     };
 
 const tabsAppender = (selector) => {
   // TASK 4
@@ -22,7 +25,21 @@ const tabsAppender = (selector) => {
   // It should obtain topics from this endpoint: `https://lambda-times-api.herokuapp.com/topics`
   // Find the array of topics inside the response, and create the tabs using the Tabs component.
   // Append the tabs to the element in the DOM that matches the selector passed to the function.
-  //
+     axios.get('https://lambda-times-api.herokuapp.com/topics') 
+          .then(returnedData => { //then give the returned data a name so we can do things with it. 
+               console.log('file: index.js ~  returnedData', returnedData);	//console log so to see what is returned   
+               parent= document.querySelector(selector);
+               parent.appendChild(Tabs(returnedData.data.topics));
+          })
+          .catch(error =>{
+               console.log('file: index.js ~ error', error);
+               })
+
+          .finally(()=>{
+               console.log("Done")     
+          });
+   
+     
 }
 
 export { Tabs, tabsAppender }
